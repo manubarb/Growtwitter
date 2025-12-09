427997b61694b609a1d506b8092f2dffa2b94220
Manuela Barba <manuelabarba.contato@gmail.com>
2025-12-08 20:25:46 -0400

    chore: adjust wrapper from LoginCard/styles.ts

--- src/components/LoginCard/styles.ts
+++ src/components/LoginCard/styles.ts
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   display: flex;
   padding: 5rem;
+  width: 60%;
+  margin: 20vh auto;
 `
 
 export const TextSection = styled.div`
