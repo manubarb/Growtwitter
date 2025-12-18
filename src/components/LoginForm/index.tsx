f29e718db8cb5bbc0a0e959f8b839bb381c496f6
Manuela Barba <manuelabarba.contato@gmail.com>
2025-12-17 20:03:47 -0400

    fix: fix input id

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,19 +1,27 @@
+import { useContext } from 'react'
+import { AuthContext } from '../../contexts/authContext'
 import { FormWrapper, Button } from './styles'
 
 export function LoginForm() {
+  const { signIn } = useContext(AuthContext)
+
+  function handleLogin() {
+    signIn()
+  }
+  
   return (
     <>
       <FormWrapper>
         <div>
         <h3>Entrar no Growtwitter</h3>
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" />
+          <input type="text" id="username" />
 
           <label htmlFor="password">Password</label>
-          <input type="text" name="password" />
+          <input type="text" id="password" />
         </div>
 
-        <Button>Entrar</Button>
+        <Button onClick={handleLogin}>Entrar</Button>
       </FormWrapper>
     </>
   )
