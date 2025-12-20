57cc262686efdf22c8b870c840423d12f64221cf
Manuela Barba <manuelabarba.contato@gmail.com>
2025-12-19 22:03:04 -0400

    feat: implement axios interceptor and auth api services

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,3 +8,16 @@ export const api = axios.create({
     'Content-Type': 'application/json'
   }
 })
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('@App:token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+export const authAPI = {
+  login: (username: string, password: string) => 
+    api.post('/auth/login', { username, password })
+}
