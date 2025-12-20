3485aefdd44cf75c44457aa61c3529e639343089
Manuela Barba <manuelabarba.contato@gmail.com>
2025-12-19 22:14:18 -0400

    feat: implement login functionality

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,27 +1,39 @@
-import { useContext } from 'react'
-import { AuthContext } from '../../contexts/authContext'
+import { useState, type FormEvent } from 'react'
+import { useAuth } from '../../hooks/useAuth'
 import { FormWrapper, Button } from './styles'
 
 export function LoginForm() {
-  const { signIn } = useContext(AuthContext)
+  const [ username, setUsername ] = useState('')
+  const [ password, setPassword ] = useState('')
 
-  function handleLogin() {
-    signIn()
+  const { signIn, isLoading } = useAuth()
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    const success = await signIn(username, password)
+    if(success){
+      console.log('logado')
+    }else{
+      console.log('erro ao logar')
+    }
   }
   
   return (
     <>
-      <FormWrapper>
+      <FormWrapper onSubmit={handleSubmit}>
         <div>
         <h3>Entrar no Growtwitter</h3>
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" />
+          <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" id="username" autoComplete='true' />
 
           <label htmlFor="password">Password</label>
-          <input type="text" id="password" />
+          <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" autoComplete='true' />
         </div>
 
-        <Button onClick={handleLogin}>Entrar</Button>
+        <Button type='submit' disabled={isLoading}>
+          Entrar
+        </Button>
       </FormWrapper>
     </>
   )
