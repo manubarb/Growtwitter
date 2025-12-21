871679a76b582d59a4f093827f06d751412e728a
Manuela Barba <manuelabarba.contato@gmail.com>
2025-12-20 23:09:33 -0400

    feat: add home route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from '../pages/Login'
+import { Home } from '../pages/Home'
 
 export function AppRoutes(){
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<Login />} />
+                <Route path="/home" element={<Home />} />
             </Routes>
         </BrowserRouter>
     )
