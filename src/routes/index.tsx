865f75333b70c1cd887d4ac67834a991d0d7112f
Manuela Barba <manuelabarba.contato@gmail.com>
2025-12-23 00:10:08 -0400

    feat: implement PrivateRoute component for authenticated routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from '../pages/Login'
 import { Home } from '../pages/Home'
+import { PrivateRoute } from '../components/PrivateRoute'
 
 export function AppRoutes(){
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<Login />} />
-                <Route path="/home" element={<Home />} />
+                <Route element={<PrivateRoute />}>
+                    <Route path="/home" element={<Home />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
