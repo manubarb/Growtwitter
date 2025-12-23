7a04bbd4c36860eb5e43dce1ad5cdaad149b45f5
Manuela Barba <manuelabarba.contato@gmail.com>
2025-12-23 17:26:42 -0400

    style: fix indentation in PublicRoute component

--- src/components/PublicRoute/index.tsx
+++ src/components/PublicRoute/index.tsx
@@ -2,5 +2,5 @@ import { Navigate, Outlet } from 'react-router-dom'
 
 export function PublicRoute() {
     const token = localStorage.getItem('@App:token')
-return token ? <Navigate to="/home" replace /> : <Outlet />
+    return token ? <Navigate to="/home" replace /> : <Outlet />
 }
