c6dff8fb1d21ee7fd59eb27b2cba3e8eb5fb44e2
Manuela Barba <manuelabarba.contato@gmail.com>
2025-12-23 17:17:26 -0400

    feat: implement PublicRoute component to prevent logged users from accessing login page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from '../pages/Login'
 import { Home } from '../pages/Home'
 import { PrivateRoute } from '../components/PrivateRoute'
+import { PublicRoute } from '../components/PublicRoute'
 
 export function AppRoutes(){
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/login" element={<Login />} />
+                <Route element={<PublicRoute />}>
+                    <Route path="/login" element={<Login />} />
+                </Route>
                 <Route element={<PrivateRoute />}>
                     <Route path="/home" element={<Home />} />
                 </Route>
