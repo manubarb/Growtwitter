b08e3f3c0977dab76061a8ab859cbf9bee652408
Manuela Barba <manuelabarba.contato@gmail.com>
2026-01-28 21:53:47 -0400

    refactor: migrate LoginCard component to mui

--- src/components/LoginCard/index.tsx
+++ src/components/LoginCard/index.tsx
@@ -1,25 +1,79 @@
-import { LoginForm } from '../LoginForm/index'
-import { Wrapper, TextSection, FormContainer } from './styles'
+import { Box, CssBaseline, Typography, Grid } from '@mui/material'
+import { LoginForm } from '../LoginForm'
 
 export function LoginCard() {
   return (
     <>
-      <Wrapper>
-        <TextSection>
-          <h1>Growtwitter</h1>
-          <small>Trabalho final do bloco intermediário</small>
-          <p>
-            O Growtwitter é a plataforma definitiva para todos os apaixonados
-            por redes sociais que buscam uma experiência familiar e poderosa,
-            semelhante ao Twitter, mas com um toque único. Seja parte desta
-            comunidade que valoriza a liberdade de expressão, a conexão com
-            pessoas de todo o mundo e a disseminação de ideias.
-          </p>
-        </TextSection>
-        <FormContainer>
-          <LoginForm />
-        </FormContainer>
-      </Wrapper>
+      <CssBaseline />
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: '100%',
+            maxWidth: 1000,
+            px: 10
+          }}
+        >
+          <Grid
+            container
+            columns={{ xs: 2, sm: 4, md: 6, lg: 12 }}
+            flexDirection={{ xs: 'column', sm: 'row', md: 'row', lg: 'row' }}
+          >
+            <Grid
+              size={6}
+              sx={{
+                height: 350,
+                backgroundColor: 'primary.main',
+                borderTopLeftRadius: { xs: 20, lg: 20 },
+                borderBottomLeftRadius: { xs: 0, lg: 20 },
+                borderTopRightRadius: { xs: 20, lg: 0 },
+                padding: '3rem',
+                display: 'flex',
+                alignItems: 'center'
+              }}
+            >
+              <Grid
+                container
+                direction="column"
+                spacing={1}
+                sx={{color: 'secondary.main'}}
+              >
+                <Typography variant="h5">Growtwitter</Typography>
+                <Typography variant="subtitle2">
+                  Trabalho final do bloco intermediário
+                </Typography>
+                <Typography variant="body1">
+                  O Growtwitter é a plataforma definitiva para todos os apaixonados
+                  por redes sociais que buscam uma experiência familiar e poderosa,
+                  semelhante ao Twitter, mas com um toque único. Seja parte desta
+                  comunidade que valoriza a liberdade de expressão, a conexão com
+                  pessoas de todo o mundo e a disseminação de ideias.
+                </Typography>
+              </Grid>
+            </Grid>
+
+            <Grid
+              size={6}
+              sx={{
+                height: 350,
+                backgroundColor: 'background.paper',
+                borderTopRightRadius: { xs: 0, lg: 20 },
+                borderBottomRightRadius: { xs: 20, lg: 20 },
+                borderBottomLeftRadius: { xs: 20, lg: 0 },
+                padding: { lg: '5rem' },
+                paddingLeft: { xs: '16rem', sm: '10rem', md: '16rem' },
+                display: 'flex',
+                alignItems: 'center'
+              }}
+            >
+              <LoginForm />
+            </Grid>
+            
+          </Grid>
+        </Box>
+ 
     </>
   )
 }
--- src/components/LoginCard/styles.ts
+++ src/components/LoginCard/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+/*import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   display: flex;
@@ -58,4 +58,4 @@ export const FormContainer = styled.div`
     width: 100%;
     height: 100%;
   }
-`
+`*/
