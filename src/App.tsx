1118af45300da7e5bd904298192272cd4532a7d8
Manuela Barba <manuelabarba.contato@gmail.com>
2026-01-28 21:57:21 -0400

    chore: implement ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { ThemeProvider } from '@mui/material'
 import { AuthProvider } from './contexts/AuthContext'
 import { AppRoutes } from './routes'
-import { GlobalStyle } from './styles/global'
+import { theme } from './themes'
+// import { GlobalStyle } from './styles/global'
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <AuthProvider>
-        <AppRoutes />
-      </AuthProvider>
+      {/* <GlobalStyle /> */}
+      <ThemeProvider theme={theme}>
+          <AuthProvider>
+            <AppRoutes />
+          </AuthProvider>
+      </ThemeProvider>
+
     </>
   )
 }
