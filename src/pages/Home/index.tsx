3a081a109a3cf8a83784f8f6166e056e1f13302a
Manuela Barba <manuelabarba.contato@gmail.com>
2026-01-30 22:24:26 -0400

    chore: build homepage base with grids

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 // deve conter o feed de tweets do usuário logado
+import { CssBaseline, Divider, Grid } from '@mui/material'
 import { Explore } from '../../components/Explore'
 import { Feed } from '../../components/Feed'
 import { SideBar } from '../../components/SideBar'
@@ -7,11 +8,55 @@ import { SideBar } from '../../components/SideBar'
 export function Home() {
   return (
     <>
-      container aqui
-        <SideBar />
-        <Feed />
-        <Explore />
-      container aqui
+      <CssBaseline />
+      <Grid
+      container
+      spacing={5}
+      columns={{ sm: 4, md: 6, lg: 12 }}
+      flexDirection='row'
+      sx={{
+        justifyContent: 'space-evenly',
+        alignItems: "stretch"
+      }}
+      >
+        <Grid
+        size={2}
+        >
+          <SideBar />
+        </Grid>
+
+        <Divider 
+        orientation="vertical" 
+        flexItem
+        sx={{
+          borderColor: 'secondary.main',
+          height: '100vh'
+        }}
+        />
+
+        <Grid
+        size={2}
+        >
+          <Feed />
+        </Grid>
+
+        <Divider 
+        orientation="vertical" 
+        flexItem
+        sx={{
+          borderColor: 'secondary.main',
+          height: '100vh'
+        }}
+        />
+
+        <Grid
+        size={2}
+        >
+          <Explore />
+        </Grid>
+
+      </Grid>
+
     </>
   )
 }
